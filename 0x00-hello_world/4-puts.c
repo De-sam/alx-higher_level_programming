bd79f17201148c14cc68bb428ee1c14786cc4213
De-sam <samsoncoded@gmail.com>
2023-07-13 13:33:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This prints Programming is like building a multilingual puzzle
+ *
+ * Return: This returns 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
