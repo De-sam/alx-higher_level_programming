dac4fe217773f1f156eb7a216c4a01fc4ccd2a71
De-sam <samsoncoded@gmail.com>
2023-07-14 15:20:21 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - This receives no argument
+ *
+ *Return: Alwaysw 0 (Success)
+ */
+int main(void)
+{
+	char alpha1, alpha2;
+
+	for (alpha1 = 'a'; alpha1 <= 'z'; alpha1++)
+	{
+		putchar(alpha1);
+	}
+
+	for (alpha2 = 'A'; alpha2 <= 'Z'; alpha2++)
+	{
+		putchar(alpha2);
+	}
+	putchar('\n');
+	return (0);
+}	
