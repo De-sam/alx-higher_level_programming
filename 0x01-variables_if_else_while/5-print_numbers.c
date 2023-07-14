e011f2e55cf84f074f10255ff34afa9d16bf6ab5
De-sam <samsoncoded@gmail.com>
2023-07-14 15:36:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - receives no argument
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
