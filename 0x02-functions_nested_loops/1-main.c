93120a74a246446723c3ec39322b0beae6bc09ff
De-sam <samsoncoded@gmail.com>
2023-07-18 08:45:40 -0700

    /alx-low_level_programming

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,12 @@
-[4:17 pm, 18/07/2023] S_m: #ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-/* Function prototype for _putchar */
-int _putchar(char c);
-
-/* Function prototype for print_alphabet */
-void print_alphabet(void);
-
-#endif /* MAIN_H */
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    print_alphabet();
+    return 0;
+}
