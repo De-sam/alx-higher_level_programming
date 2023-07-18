b2ed254da7d2ad0d9f682e7fbe18046363da3179
De-sam <samsoncoded@gmail.com>
2023-07-18 11:40:15 -0700

    beud function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,10 +10,13 @@ int _isalpha(int c);
 /*Function prototype for print_sign*/
 int print_sign(int n);
 
-/*Function prototype for print_sign*/                                           
+/*Function prototype _abs*/                                           
 int _abs(int n);
 
-/*Function prototype for print_sign*/                                           
- int print_last_digit(int n);
+/*Function prototype for last_digit*/
+int print_last_digit(int n);
+
+/*Function prototype for jack_beauer*/                                           
+ void jack_bauer(void);
 
 #endif /* MAIN_H */
