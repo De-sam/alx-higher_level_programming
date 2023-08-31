f0984fc1057968e1a995a97458161d8264567486
De-sam <samsoncoded@gmail.com>
2023-08-31 20:21:39 +0100

    fixing errors in bit manipulation task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: the converted numbers, or
- * 0 if there is no one or
- *  more chars in the string b that is not 0 or 1 or b is NULL
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: A pointer to a string of 0 and 1 chars
+ * Return: Converted numbers, or 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
