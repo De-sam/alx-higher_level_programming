bb293abc3ecd42a4962588de58156d62cc6c70c7
De-sam <samsoncoded@gmail.com>
2023-10-11 16:18:33 +0100

    matrix squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 from math import pow
 def square_matrix_simple(matrix=[]):
-	for sublist in matrix:
-		for num in range(len(sublist)):
-			sublist[num] **= 2
-	return matrix		
+	new_matrix = [[num ** 2 for num in sublist] for sublist in matrix]
+	return new_matrix
+	
